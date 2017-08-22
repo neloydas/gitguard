798794b4b3407f33bb14ae3a5e341c891772103b
neloydas <niloy.sunny+github@gmail.com>
2017-08-22 18:19:09 +0200

    Update disabler.js

--- src/disabler.js
+++ src/disabler.js
@@ -1,7 +1,7 @@
 var mergeButtons = document.getElementsByClassName('btn btn-primary BtnGroup-item js-details-target');
 
-if(mergeButtons.length == 3) {
+//if(mergeButtons.length == 3) {
 	mergeButtons[0].disabled = true;
 	mergeButtons[1].disabled = true;
 	mergeButtons[2].disabled = true;
-}
+//}
