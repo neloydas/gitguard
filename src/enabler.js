aef937cf38b01df733d8b66ac20d10309f70823b
neloydas <niloy.sunny+github@gmail.com>
2017-08-22 16:13:11 +0200

    Update enabler.js

--- src/enabler.js
+++ src/enabler.js
@@ -1,10 +1,11 @@
 var mergeButtons = document.getElementsByClassName('btn btn-primary js-merge-branch-action');
 
-if(mergeButtons.length != 1) {
-	mergeButtons = document.getElementsByClassName('btn btn-danger js-merge-branch-action');
-}
+//if(mergeButtons.length != 1) {
+//	mergeButtons = document.getElementsByClassName('btn btn-danger js-merge-branch-action');
+//}
 
-if (mergeButtons.length == 1) {
-	var mergeButton = mergeButtons[0];
-	mergeButton.disabled = false;
+if (mergeButtons.length == 3) {
+	mergeButtons[0].disabled = false;
+	mergeButtons[1].disabled = false;
+	mergeButtons[2].disabled = false;
 }
